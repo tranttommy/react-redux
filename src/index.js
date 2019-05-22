e57629763fb1f2fc3843ff9c6cc5b6c92c5d70ba
Tommy Tran <tranttommy95@gmail.com>
2019-05-22 14:50:35 -0700

    did the exercise

--- src/index.js
+++ src/index.js
@@ -1,8 +1,79 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
 
-render(
-  <App />,
-  document.getElementById('root')
-);
+const initialState = {
+  sandwiches: [],
+  drinks: [],
+  chips: []
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_DRINK':
+      return { ...state, drinks: [...state.drinks, action.payload] };
+    case 'REMOVE_DRINKS':
+      return { ...state, drinks: [] };
+    case 'ADD_SANDWICH':
+      return { ...state, sandwiches: [...state.sandwiches, action.payload] };
+    case 'REMOVE_SANDWICHES':
+      return { ...state, sandwiches: [] };
+    case 'ADD_CHIP':
+      return { ...state, chips: [...state.chips, action.payload] };
+    case 'REMOVE_CHIPS':
+      return { ...state, chips: [] };
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'ADD_DRINK',
+  payload: 'soda'
+});
+console.log('1 drink', store.getState());
+
+store.dispatch({
+  type: 'ADD_DRINK',
+  payload: 'juice'
+});
+console.log('2 drinks', store.getState());
+
+store.dispatch({
+  type: 'ADD_SANDWICH',
+  payload: 'tuna'
+});
+console.log('1 sandwich', store.getState());
+
+store.dispatch({
+  type: 'ADD_SANDWICH',
+  payload: 'lgbt'
+});
+console.log('2 sandwiches', store.getState());
+
+store.dispatch({
+  type: 'ADD_CHIP',
+  payload: 'crunchy'
+});
+console.log('1 chip', store.getState());
+
+store.dispatch({
+  type: 'ADD_CHIP',
+  payload: 'cool ranch'
+});
+console.log('2 chips', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_DRINKS'
+});
+console.log('no drinks', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_SANDWICHES'
+});
+console.log('no sandwiches', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_CHIPS'
+});
+console.log('no chips', store.getState());
