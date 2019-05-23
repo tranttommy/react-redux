8ef51771b04fa2abb09de7547bdb2e586ab45e45
Tommy Tran <tranttommy95@gmail.com>
2019-05-23 16:05:34 -0700

    refactored ADD_COMMENT and made DELETE_COMMENT with test passing

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,10 +1,18 @@
-import { ADD_COMMENT } from '../actions/commentActions';
+import { ADD_COMMENT, DELETE_COMMENT } from '../actions/commentActions';
 
-const initialState = [];
+const initialState = {};
 export default function commentReducer(state = initialState, action) {
   switch(action.type) {
     case ADD_COMMENT:
-      return [...state, action.payload];
+      return {
+        ...state,
+        [action.payload.postId]: state[action.payload.postId] ? [...(state[action.payload.postId]), action.payload.body] : [action.payload.body]
+      };
+    case DELETE_COMMENT:
+      return {
+        ...state,
+        [action.payload.postId]: state[action.payload.postId].filter((_, i) => i !== action.payload.commentIndex)
+      };
     default:
       return state;
   }
--- src/reducers/commentReducer.test.js
+++ src/reducers/commentReducer.test.js
@@ -1,20 +1,31 @@
 import commentReducer from './commentReducer';
-import { addComment } from '../actions/commentActions';
+import { addComment, deleteComment } from '../actions/commentActions';
 
 describe('commentReducer', () => {
   it('returns new state with added comment', () => {
-    const initialState = [];
+    const initialState = {};
     const comment = {
       postId: 23,
       body: 'Helloe'
     };
     const newState = commentReducer(initialState, addComment(comment));
-    expect(initialState).toEqual([]);
-    expect(newState).toEqual([
-      {
-        postId: 23,
-        body: 'Helloe'
-      }
-    ]);
+    expect(initialState).toEqual({});
+    expect(newState).toEqual({
+      '23': ['Helloe']
+    });
+  });
+
+  it('returns new state with delete comment', () => {
+    const initialState = {
+      '23': ['Comment1', 'Comment2', 'Comment3', 'Comment4']
+    };
+    const newState = commentReducer(initialState, deleteComment(23, 1));
+    expect(newState).toEqual({
+      '23': ['Comment1', 'Comment3', 'Comment4']
+    });
+    const newerState = commentReducer(newState, deleteComment(23, 1));
+    expect(newerState).toEqual({
+      '23': ['Comment1', 'Comment4']
+    });
   });
 });
