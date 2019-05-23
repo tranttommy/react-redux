a443201f0e67dce3eeaac3e0835bc62f40155778
Tommy Tran <tranttommy95@gmail.com>
2019-05-23 16:48:59 -0700

    added delete post

--- src/reducers/commentReducer.js
+++ src/reducers/commentReducer.js
@@ -1,4 +1,5 @@
 import { ADD_COMMENT, DELETE_COMMENT } from '../actions/commentActions';
+import { DELETE_POST } from '../actions/postActions';
 
 const initialState = {};
 export default function commentReducer(state = initialState, action) {
@@ -13,6 +14,11 @@ export default function commentReducer(state = initialState, action) {
         ...state,
         [action.payload.postId]: state[action.payload.postId].filter((_, i) => i !== action.payload.commentIndex)
       };
+    case DELETE_POST: {
+      // eslint-disable-next-line no-unused-vars
+      const { [action.payload]: _, ...newState } = state;
+      return newState;
+    }
     default:
       return state;
   }
