401d91227f75220feb3394eb64ac494b6f6d17a2
Tommy Tran <tranttommy95@gmail.com>
2019-05-23 16:49:18 -0700

    made store with some console.log test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
+import reducer from './reducers';
+import { createPost, deletePost } from './actions/postActions';
+import { addComment } from './actions/commentActions';
 
-render(
-  <App />,
-  document.getElementById('root')
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+console.log(store.getState());
+
+store.dispatch(createPost({
+  id: 12,
+  title: 'Title',
+  body: 'Body'
+}));
+console.log(store.getState());
+
+
+store.dispatch(deletePost(12));
+console.log(store.getState());
+
+store.dispatch(createPost({
+  id: 12,
+  title: 'Title',
+  body: 'Body'
+}));
+
+store.dispatch(createPost({
+  id: 13,
+  title: 'Title',
+  body: 'Body'
+}));
+
+store.dispatch(addComment({
+  postId: 12,
+  body: 'Comment1'
+}));
+
+store.dispatch(addComment({
+  postId: 12,
+  body: 'Comment2'
+}));
+console.log(store.getState());
+
+store.dispatch(deletePost(12));
+console.log(store.getState());
