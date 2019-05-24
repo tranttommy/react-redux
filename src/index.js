672b9466d5d00f2a0e4b209f0bf81e86847d1028
Tommy Tran <tranttommy95@gmail.com>
2019-05-24 10:08:17 -0700

    installed react-redux, setup store + index.js

--- src/index.js
+++ src/index.js
@@ -1,48 +1,12 @@
-import { createStore } from 'redux';
-import reducer from './reducers';
-import { createPost, deletePost } from './actions/postActions';
-import { addComment } from './actions/commentActions';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import App from './components/App';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
-
-console.log(store.getState());
-
-store.dispatch(createPost({
-  id: 12,
-  title: 'Title',
-  body: 'Body'
-}));
-console.log(store.getState());
-
-
-store.dispatch(deletePost(12));
-console.log(store.getState());
-
-store.dispatch(createPost({
-  id: 12,
-  title: 'Title',
-  body: 'Body'
-}));
-
-store.dispatch(createPost({
-  id: 13,
-  title: 'Title',
-  body: 'Body'
-}));
-
-store.dispatch(addComment({
-  postId: 12,
-  body: 'Comment1'
-}));
-
-store.dispatch(addComment({
-  postId: 12,
-  body: 'Comment2'
-}));
-console.log(store.getState());
-
-store.dispatch(deletePost(12));
-console.log(store.getState());
